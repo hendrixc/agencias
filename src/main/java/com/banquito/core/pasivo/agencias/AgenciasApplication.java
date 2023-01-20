d0ff255361ac52323ad6beb425c00496ed0d9b12
Hendrix <hrcoral@espe.edu.ec>
2023-01-20 07:35:11 -0500

    Initial commit

---
+++ src/main/java/com/banquito/core/pasivo/agencias/AgenciasApplication.java
@@ -0,0 +1,13 @@
+package com.banquito.core.pasivo.agencias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgenciasApplication.class, args);
+	}
+
+}
